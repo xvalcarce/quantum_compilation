e3d86f87da867df49c64e3cc19d2fae0ceac3421
Xavier Valcarce <xvalcarce@protonmail.ch>
2024-12-04 12:07:22 +0100

    add: dependencies setup.py

--- setup.py
+++ setup.py
@@ -6,5 +6,8 @@ setup(
     packages=find_packages(),
     include_package_data=True,
     install_requires=[ 
+                      pgx,
+                      jax,
+                      qujax,
     ],
 )
