2022e53d430c6bc27977ccfd3a1f3c2479cc1803
Xavier Valcarce <xvalcarce@protonmail.ch>
2024-12-19 15:13:12 +0100

    ix: correct dependencies

--- setup.py
+++ setup.py
@@ -6,8 +6,8 @@ setup(
     packages=find_packages(),
     include_package_data=True,
     install_requires=[ 
-                      pgx,
-                      jax,
-                      qujax,
+                      "pgx",
+                      "jax",
+                      "qujax",
     ],
 )
